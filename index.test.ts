d19a68283ef93a655ab18487321d2c6c5a994a1b
Alasdair McLeay <alasdair.mcleay@me.com>
2022-02-23 18:17:44 +0000

    Initial commit

---
+++ index.test.ts
@@ -0,0 +1,235 @@
+import {renderHook, act} from '@testing-library/react-hooks';
+import useQuery from '.';
+
+class Deferred<T> {
+  promise: Promise<T>;
+  resolve: (value: T | PromiseLike<T>) => void = () => null;
+  reject: (reason?: any) => void = () => null;
+
+  constructor() {
+    this.promise = new Promise((resolve, reject) => {
+      this.resolve = resolve;
+      this.reject = reject;
+    });
+  }
+}
+
+describe('useQuery', () => {
+  it('should handle queries completing in series', async () => {
+    const deferred1 = new Deferred();
+    const deferred2 = new Deferred();
+
+    const mockAPI = jest.fn();
+    const query = (variables: string) => {
+      mockAPI(variables);
+      if (variables === 'run1') {
+        return deferred1.promise;
+      } else {
+        return deferred2.promise;
+      }
+    };
+
+    let {result, rerender} = renderHook(options => useQuery(query, options), {
+      initialProps: {variables: 'run1'},
+    });
+
+    expect(mockAPI).toHaveBeenCalledWith('run1');
+    expect(result.current.error).toBe(null);
+    expect(result.current.loading).toBe(true);
+    expect(result.current.data).toBe(null);
+
+    await act(async () => {
+      deferred1.resolve(1);
+    });
+
+    expect(result.current.error).toBe(null);
+    expect(result.current.loading).toBe(false);
+    expect(result.current.data).toBe(1);
+
+    rerender({variables: 'run2'});
+    expect(mockAPI).toHaveBeenCalledWith('run2');
+
+    expect(result.current.error).toBe(null);
+    expect(result.current.loading).toBe(true);
+    expect(result.current.data).toBe(null);
+
+    await act(async () => {
+      deferred2.resolve(2);
+    });
+
+    expect(result.current.error).toBe(null);
+    expect(result.current.loading).toBe(false);
+    expect(result.current.data).toBe(2);
+  });
+
+  it('should handle parallel queries completing in order', async () => {
+    const deferred1 = new Deferred();
+    const deferred2 = new Deferred();
+
+    const mockAPI = jest.fn();
+    const query = (variables: string) => {
+      mockAPI(variables);
+      if (variables === 'run1') {
+        return deferred1.promise;
+      } else {
+        return deferred2.promise;
+      }
+    };
+
+    let {result, rerender} = renderHook(options => useQuery(query, options), {
+      initialProps: {variables: 'run1'},
+    });
+
+    expect(mockAPI).toHaveBeenCalledWith('run1');
+    expect(result.current.error).toBe(null);
+    expect(result.current.loading).toBe(true);
+    expect(result.current.data).toBe(null);
+
+    rerender({variables: 'run2'});
+
+    expect(mockAPI).toHaveBeenCalledWith('run2');
+    expect(result.current.error).toBe(null);
+    expect(result.current.loading).toBe(true);
+    expect(result.current.data).toBe(null);
+
+    await act(async () => {
+      deferred1.resolve(1);
+    });
+
+    expect(result.current.error).toBe(null);
+    expect(result.current.loading).toBe(true);
+    expect(result.current.data).toBe(null);
+
+    await act(async () => {
+      deferred2.resolve(2);
+    });
+
+    expect(result.current.error).toBe(null);
+    expect(result.current.loading).toBe(false);
+    expect(result.current.data).toBe(2);
+  });
+
+  it('should handle parallel queries completing in reverse order', async () => {
+    const deferred1 = new Deferred();
+    const deferred2 = new Deferred();
+
+    const mockAPI = jest.fn();
+    const query = (variables: string) => {
+      mockAPI(variables);
+      if (variables === 'run1') {
+        return deferred1.promise;
+      } else {
+        return deferred2.promise;
+      }
+    };
+
+    let {result, rerender} = renderHook(options => useQuery(query, options), {
+      initialProps: {variables: 'run1'},
+    });
+
+    expect(mockAPI).toHaveBeenCalledWith('run1');
+    expect(result.current.error).toBe(null);
+    expect(result.current.loading).toBe(true);
+    expect(result.current.data).toBe(null);
+
+    rerender({variables: 'run2'});
+
+    expect(mockAPI).toHaveBeenCalledWith('run2');
+    expect(result.current.error).toBe(null);
+    expect(result.current.loading).toBe(true);
+    expect(result.current.data).toBe(null);
+
+    await act(async () => {
+      deferred2.resolve(2);
+    });
+
+    expect(result.current.error).toBe(null);
+    expect(result.current.loading).toBe(false);
+    expect(result.current.data).toBe(2);
+
+    await act(async () => {
+      deferred1.resolve(1);
+    });
+
+    expect(result.current.error).toBe(null);
+    expect(result.current.loading).toBe(false);
+    expect(result.current.data).toBe(2);
+  });
+
+  it('reports an error if query fails', async () => {
+    const deferred1 = new Deferred();
+
+    const mockAPI = jest.fn();
+    const errorLog = jest.fn();
+    const query = (variables: string) => {
+      mockAPI(variables);
+      return deferred1.promise;
+    };
+
+    let {result} = renderHook(options => useQuery(query, options), {
+      initialProps: {variables: 'run1', onError: errorLog},
+    });
+
+    expect(mockAPI).toHaveBeenCalledWith('run1');
+    expect(result.current.error).toBe(null);
+    expect(result.current.loading).toBe(true);
+    expect(result.current.data).toBe(null);
+
+    await act(async () => {
+      deferred1.reject('some error');
+    });
+
+    expect(result.current.error).toBe('some error');
+    expect(errorLog).toHaveBeenCalledTimes(1);
+  });
+
+  it("doesn't report an error if a new query has started after a query fails", async () => {
+    const deferred1 = new Deferred();
+    const deferred2 = new Deferred();
+
+    const mockAPI = jest.fn();
+    const errorLog = jest.fn();
+    const query = (variables: string) => {
+      mockAPI(variables);
+      if (variables === 'run1') {
+        return deferred1.promise;
+      } else {
+        return deferred2.promise;
+      }
+    };
+
+    let {result, rerender} = renderHook(options => useQuery(query, options), {
+      initialProps: {variables: 'run1', onError: errorLog},
+    });
+
+    expect(mockAPI).toHaveBeenCalledWith('run1');
+    expect(result.current.error).toBe(null);
+    expect(result.current.loading).toBe(true);
+    expect(result.current.data).toBe(null);
+
+    rerender({variables: 'run2', onError: errorLog});
+
+    expect(mockAPI).toHaveBeenCalledWith('run2');
+    expect(result.current.error).toBe(null);
+    expect(result.current.loading).toBe(true);
+    expect(result.current.data).toBe(null);
+
+    await act(async () => {
+      deferred1.reject('an error');
+    });
+
+    expect(result.current.error).toBe(null);
+    expect(result.current.loading).toBe(true);
+    expect(result.current.data).toBe(null);
+
+    await act(async () => {
+      deferred2.resolve(2);
+    });
+
+    expect(result.current.error).toBe(null);
+    expect(result.current.loading).toBe(false);
+    expect(result.current.data).toBe(2);
+
+    expect(errorLog).toHaveBeenCalledTimes(0);
+  });
+});
---
+++ index.ts
@@ -0,0 +1,59 @@
+import {useState, useEffect} from 'react';
+
+/**
+ * Mirrors the functionality of
+ * [Apollo's useQuery hook](https://www.apollographql.com/docs/react/data/queries/#usequery-api),
+ * but with a "query" being any async function rather than GQL statement.
+ */
+const useQuery = <Data, Variables>(
+  query: (variables: Variables) => Promise<Data>,
+  options: {
+    variables: Variables; //TODO: call signature without Variables, optional Options
+    skip?: boolean;
+    onCompleted?: (data: Data) => void;
+    onError?: (error: Error) => void;
+  },
+): {loading: boolean; error: Error | null; data: Data | null} => {
+  const [data, setData] = useState<Data | null>(null);
+  const [loading, setLoading] = useState<boolean>(false);
+  const [error, setError] = useState(null);
+
+  const {variables, skip, onCompleted, onError} = options || {};
+
+  useEffect(() => {
+    let isLatest = true;
+    if (skip) {
+      setLoading(false);
+      setError(null);
+    } else {
+      const promise = query(variables);
+      setLoading(true);
+      setData(null);
+      setError(null);
+      promise
+        .then(response => {
+          if (isLatest) {
+            setData(response);
+            setLoading(false);
+            setError(null);
+            onCompleted?.(response);
+          }
+        })
+        .catch(e => {
+          if (isLatest) {
+            setData(null);
+            setLoading(false);
+            setError(e);
+            onError?.(e);
+          }
+        });
+    }
+    return () => {
+      isLatest = false;
+    };
+  }, [query, variables, onCompleted, onError, skip]);
+
+  return {loading, error, data};
+};
+
+export default useQuery;
