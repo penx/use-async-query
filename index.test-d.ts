a5e0f14d5527def105b2db38ff465baaa68fa830
Alasdair McLeay <alasdair.mcleay@me.com>
2022-03-07 23:44:20 +0000

    Add tsd

---
+++ index.test-d.ts
@@ -0,0 +1,16 @@
+import { expectType } from "tsd";
+import { useAsyncQuery } from "./index";
+
+expectType<{
+  foo: string;
+} | null>(
+  useAsyncQuery<{ foo: string }, { bar: string }>(
+    () =>
+      new Promise<{ foo: string }>((resolve) =>
+        setTimeout(() => resolve({ foo: "test" }), 0)
+      ),
+    {
+      variables: { bar: "test" },
+    }
+  ).data
+);
